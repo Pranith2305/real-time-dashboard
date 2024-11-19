407d127724c53b4bb442387e23be51ac3326090e
pranithaiwiz <pranithkumar.8802@gmail.com>
2024-11-19 10:32:47 +0530

    components added

---
+++ rtd/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const home = () => {
+  return (
+    <div><Button>click me </Button></div>
+  )
+}
+
+export default home
